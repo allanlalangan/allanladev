7b2df638df0d7abc1289a9fc087d9d2e9ac98545
allanlalangan <allanlalangan@gmail.com>
2023-10-05 21:05:59 -0700

    feat: grid, sidebar, mobile layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,14 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateRows: {
+				8: 'repeat(8, 1fr)',
+				10: 'repeat(10, 1fr)',
+				12: 'repeat(12, 1fr)',
+				24: 'repeat(24, 1fr)',
+			},
+		},
 	},
 	plugins: [],
 };
